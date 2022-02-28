f1ab2aa1f143b8ce5d21bef52d891958a73024da
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-02-28 18:29:03 +0100

    feat: this adds the transfers controller

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 //@ts-check
 require("dotenv").config();
+// @ts-ignore
 require("express-async-errors");
 const http = require("http");
+// @ts-ignore
 const express = require("express");
 
 const app = express();
@@ -15,22 +17,29 @@ const connectDB = require("./db/index");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// @ts-ignore
 app.use(express.json());
+// @ts-ignore
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
+// @ts-ignore
 app.get("/", (req, res) => {
   res.send("Ledger");
 });
 
+// @ts-ignore
 app.use("/api/v1/accounts", accountRouter);
+// @ts-ignore
 app.use("/api/v1/transfers", transfersRouter);
 
 // middleware
+// @ts-ignore
 app.use(notFoundMiddleware);
+// @ts-ignore
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
@@ -39,6 +48,7 @@ const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
 
+    // @ts-ignore
     const server = http.createServer(app);
     server.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
--- routes/transfers.js
+++ routes/transfers.js
@@ -3,8 +3,13 @@ const express = require("express");
 
 const transactionRouter = express.Router();
 
-const { transfer } = require("../controllers/transfers.controller");
+const {
+  transfer,
+  deposit,
+  withdrawal,
+} = require("../controllers/transfers.controller");
 
 transactionRouter.post("/", transfer);
-
+transactionRouter.post("/deposit", deposit)
+transactionRouter.post("/withdrawal", withdrawal)
 module.exports = transactionRouter;
