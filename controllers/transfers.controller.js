51145ed9e89f0bd6f213abb00f7b447c72936627
Emmanuel Tukpe <tukpeemmanuel@gmail.com>
2022-03-01 17:42:19 +0100

    feat: this makes each function perform 1 function

--- controllers/transfers.controller.js
+++ controllers/transfers.controller.js
@@ -18,30 +18,37 @@ const credit = async (account_number, amount) => {
   );
 };
 
+/**
+ * Debits an account by an amount
+ *
+ * @param {String} account_number the account number of the account to be debited
+ * @param {Number} amount the amount to be debited
+ */
+ const debit = async (account_number, amount) => {
+  return await Accounts.findOneAndUpdate(
+    { account_number },
+    { $inc: { account_balance: -amount } },
+    { new: true }
+  );
+};
 
 
 /**
  * Checks if the amount requested in the account
- * If it is not there, it throws an error, if it is, it is debited
- * It debits the account using the account number
+ * If it is not there, it throws an error
  * @param {String} account_number the account number of the account
  * @param {Number} amount the amount to be debited
  */
-const debit = async (account_number, amount) => {
+const checksAccount = async (account_number, amount) => {
   const account = await Accounts.findOne({ account_number });
   if (account.account_balance < amount) {
     throw new BadRequestError("Insufficient Funds");
-  } else {
-    return await Accounts.findOneAndUpdate(
-      { account_number },
-      { $inc: { account_balance: -amount } },
-      { new: true }
-    );
   }
 };
 
 const transfer = async (req, res) => {
-  await debit(req.body.sender, req.body.amount);
+  await checksAccount(req.body.sender, req.body.amount);
+  debit(req.body.sender, req.body.amount);
   credit(req.body.recipient, req.body.amount);
   const transaction = await Transaction.create(req.body);
 
@@ -54,10 +61,11 @@ const deposit = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ transaction });
 };
 
-const withdrawal = async (req, res) => {
-  await debit(req.body.sender, req.body.amount);
-  const transaction = await Transaction.create(req.body);
+const withdrawal = async (req,res) => {
+  await checksAccount(req.body.sender, req.body.amount);
+  debit(req.body.sender, req.body.amount)
+  const transaction = await Transaction.create(req.body)
   res.status(StatusCodes.CREATED).json({ transaction });
-};
+}
 
 module.exports = { transfer, deposit, withdrawal };
